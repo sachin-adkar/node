b3045fcb4fec6eb09cdb02b1bdedd2932c3b196d
sachinadkar <sachin.adkarr@gmail.com>
2018-01-03 15:13:41 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -8,24 +8,39 @@ const publicPath = path.join(__dirname, '../public');
 const app = express();
 const port = process.env.PORT || 3000;
 
-var server = http.createServer(app); 
+var server = http.createServer(app);
 
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {  
+io.on('connection', (socket) => {
     console.log('New user connected');
 
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log("New message", message);
-
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
 
@@ -37,7 +52,4 @@ io.on('connection', (socket) => {
 
 server.listen(3000, () => {
     console.log('Server is running on port ', port);
-
-})
-
-
+});
